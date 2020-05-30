db9492d3870aeabb3f02ad8ac8f50000ffe4f467
Jordan Mance <mancej@snagajob.com>
2020-05-30 16:52:25 -0400

    Skipping events not from current account for cache manager

--- terraform/figgy/lambdas/functions/config_cache_manager.py
+++ terraform/figgy/lambdas/functions/config_cache_manager.py
@@ -17,9 +17,16 @@ ssm_client = boto3.client('ssm')
 ssm = SsmDao(ssm_client)
 webhook_url = ssm_dao.get_parameter_value(FIGGY_WEBHOOK_URL_PATH)
 slack: SlackService = SlackService(webhook_url=webhook_url)
+ACCOUNT_ID = ssm.get_parameter_value(ACCOUNT_ID_PS_PATH)
 
 
 def handle(event, context):
+    # Don't process other account's events.
+    originating_account = event.get('account')
+    if originating_account != ACCOUNT_ID:
+        log.info(f"Received event from different account with id: {ACCOUNT_ID}. Skipping this event.")
+        return
+
     try:
         log.info(f"Event: {event}")
         cache_dao: ConfigCacheDao = ConfigCacheDao(dynamo_resource)
