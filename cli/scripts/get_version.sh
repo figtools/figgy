80e333f74353f099c78d000601d1b9c0968c89f1
Jordan Mance <mancej@snagajob.com>
2020-06-21 17:12:22 -0400

    Fixing get-version script

--- cli/scripts/get_version.sh
+++ cli/scripts/get_version.sh
@@ -2,4 +2,4 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cat "$SCRIPTPATH/../figgy/config/constants.py"| grep -E "VERSION\s+=\s+" | grep -v "AUDIT" | sed -E "s#.*([0-9]+.[0-9]+.[0-9]+[a-zA-Z]?).*#\1#g"
+cat "$SCRIPTPATH/../figcli/config/constants.py"| grep -E "VERSION\s+=\s+" | grep -v "AUDIT" | sed -E "s#.*([0-9]+.[0-9]+.[0-9]+[a-zA-Z]?).*#\1#g"
