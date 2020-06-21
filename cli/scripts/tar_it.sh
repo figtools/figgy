62d1d7326b0a8ecd07a048bd21d3e201fc228de9
Jordan Mance <mancej@snagajob.com>
2020-06-21 19:53:20 -0400

    Testing new tar packaging for homebrew

--- cli/scripts/tar_it.sh
+++ cli/scripts/tar_it.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+version=$1
 
-cd dist/__main__/
-tar -czvf figgy.tar.gz .
-mv figgy.tar.gz ../../
+echo "Making tar with version: $version"
+
+mkdir -p "figgy/${version}/bin"
+
+cp -R dist/__main__/* "figgy/${version}/bin/"
+tar -czvf figgy.tar.gz figgy/${version}/bin
