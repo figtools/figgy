2ccfbd854ddb86bb274dcef4aa71c5da6c9c0b91
Jordan Mance <mancej@snagajob.com>
2020-06-22 08:42:34 -0400

    Improving tar script logging

--- cli/scripts/tar_it.sh
+++ cli/scripts/tar_it.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+. scripts/utils.sh
+
 version=$1
 
-echo "Making tar with version: $version"
+[[ -z "$version" ]] && die "Required parameter [version] is missing." || success "Making tar with version: $version"
 
 mkdir -p "figgy/${version}/bin"
 
 cp -R dist/__main__/* "figgy/${version}/bin/"
-tar -czvf figgy.tar.gz figgy/${version}/bin
+
+tar -czvf figgy.tar.gz "figgy/${version}/bin/"
