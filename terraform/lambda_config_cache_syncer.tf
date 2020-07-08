dea805c43164b334d4911498afbcbed6ae82c3da
Jordan Mance <mancej@snagajob.com>
2020-07-07 20:47:25 -0400

    Increasing timeout for cache syncer lambda

--- terraform/lambda_config_cache_syncer.tf
+++ terraform/lambda_config_cache_syncer.tf
@@ -4,7 +4,7 @@ module "config_cache_syncer" {
   description = "Incrementally synchronizes the cache table used for auto-complete in the figgy CLI tool"
   handler = "functions/config_cache_syncer.handle"
   lambda_name = "figgy-config-cache-syncer"
-  lambda_timeout = 60
+  lambda_timeout = 500
   policies = [aws_iam_policy.config_cache_manager.arn, aws_iam_policy.lambda_default.arn, aws_iam_policy.lambda_read_configs.arn]
   zip_path = data.archive_file.figgy.output_path
   layers = [local.aws_sdk_layer_map[var.region]]
