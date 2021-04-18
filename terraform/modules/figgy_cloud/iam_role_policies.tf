f9d3b87ca626f6af2a155c0c28721d9c2c95e42a
Jordan Mance <mancej@snagajob.com>
2021-04-17 21:32:48 -0400

    Iterating on new config-usage-tracker ddb table

--- terraform/modules/figgy_cloud/iam_role_policies.tf
+++ terraform/modules/figgy_cloud/iam_role_policies.tf
@@ -113,7 +113,8 @@ data "aws_iam_policy_document" "dynamic_policy" {
       aws_dynamodb_table.config_replication.arn,
       aws_dynamodb_table.config_auditor.arn,
       aws_dynamodb_table.config_cache.arn,
-      aws_dynamodb_table.config_usage_tracker.arn
+      aws_dynamodb_table.config_usage_tracker.arn,
+      "${aws_dynamodb_table.config_usage_tracker.arn}/*"
     ]
   }
 
