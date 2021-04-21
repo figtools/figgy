92953fee1105ccea11ed80fe19f6c12b93885a3d
Jordan Mance <mancej@snagajob.com>
2021-04-21 17:49:09 -0400

    Multi-region support - step 1

--- terraform/modules/figgy_cloud/lambda_locals.tf
+++ terraform/modules/figgy_cloud/lambda_locals.tf
@@ -1,14 +1,14 @@
 locals {
-  config_auditor_name = 'figgy-config-auditor'
-  config_cache_manager_name = 'figgy-config-cache-manager'
-  config_cache_syncer_name = 'figgy-config-cache-syncer'
-  config_usage_tracker_name = 'figgy-config-usage-tracker'
-  dynamo_stream_replicator_name = 'figgy-dynamo-stream-replicator'
-  replication_syncer_name = 'figgy-replication-syncer'
-  ssm_stream_replicator_name = 'figgy-ssm-stream-replicator'
+  config_auditor_name = "figgy-config-auditor"
+  config_cache_manager_name = "figgy-config-cache-manager"
+  config_cache_syncer_name = "figgy-config-cache-syncer"
+  config_usage_tracker_name = "figgy-config-usage-tracker"
+  dynamo_stream_replicator_name = "figgy-dynamo-stream-replicator"
+  replication_syncer_name = "figgy-replication-syncer"
+  ssm_stream_replicator_name = "figgy-ssm-stream-replicator"
 
   # POLICIES
-  lambda_default_policy_name = 'figgy-lambda-default'
-  config_replication_policy_name = 'config-replication'
-  read_figgy_configs_policy_name = 'figgy-lambda-read-figgy-specific-configs'
+  lambda_default_policy_name = "figgy-lambda-default"
+  config_replication_policy_name = "config-replication"
+  read_figgy_configs_policy_name = "figgy-lambda-read-figgy-specific-configs"
 }
