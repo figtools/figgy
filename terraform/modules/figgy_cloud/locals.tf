dcd3ffc0f08ee310c83cf50ec230c71ee5378f9d
Jordan Mance <mancej@snagajob.com>
2021-04-22 16:30:09 -0400

    Working to add multi-region support

--- terraform/modules/figgy_cloud/locals.tf
+++ terraform/modules/figgy_cloud/locals.tf
@@ -10,6 +10,6 @@ locals {
   # The figgy sandbox allows role assumption by accountId and by RoleId. This is unique to the figgy sandbox.
   principals = var.sandbox_deploy ? var.cfgs.sandbox_principals : var.cfgs.bastion_principal
   region = data.aws_region.current.name
-  account_id = data.aws_caller_identity.current.account_id
+  account_id = var.aws_account_id
 }
 
