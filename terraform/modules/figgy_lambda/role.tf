3e0e41bbc5d3d7ac3f69639ccbee8ff4002ec9bf
Jordan Mance <mancej@snagajob.com>
2021-04-22 09:23:09 -0400

    Working to add multi-region support

--- terraform/modules/figgy_lambda/role.tf
+++ terraform/modules/figgy_lambda/role.tf
@@ -5,11 +5,12 @@ data "aws_region" "current" {}
 
 
 locals {
+  role_name = "${var.lambda_name}-${data.aws_region.current.name}"
   role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.lambda_name}"
 }
 
 resource "aws_iam_role" "figgy_role" {
-  name = "${var.lambda_name}-${data.aws_region.current.name}"
+  name = local.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_policy.json
 }
 
