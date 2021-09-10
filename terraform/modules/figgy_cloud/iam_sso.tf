8c34a73b88bc39a3ff4852c49d2d3cbeed1b86b9
Jordan Mance <mancej@snagajob.com>
2021-09-10 10:53:20 -0400

    Adding ots role

--- terraform/modules/figgy_cloud/iam_sso.tf
+++ terraform/modules/figgy_cloud/iam_sso.tf
@@ -20,10 +20,12 @@ resource "aws_iam_role" "sso_user_role" {
   max_session_duration = var.max_session_duration
 }
 
-resource "aws_iam_role" "ots_role" {
+# Used to provide generic figgy access to its utilities. Does not provide access to any secrets, keys, or environments.
+# All users of figgy should have this role
+resource "aws_iam_role" "default_role" {
   count                = var.cfgs.utility_account_id == var.aws_account_id ? 1 : 0
-  name                 = "figgy-ots"
-  assume_role_policy   = data.aws_iam_policy_document.sso_role_policy[count.index].json
+  name                 = "figgy-default"
+  assume_role_policy   = var.cfgs.enable_sso ? data.aws_iam_policy_document.sso_role_policy[0].json : ""
   max_session_duration = var.max_session_duration
 }
 
@@ -58,6 +60,6 @@ resource "aws_iam_role_policy_attachment" "figgy_access_policy_attachment" {
 
 resource "aws_iam_role_policy_attachment" "figgy_ots_policy_attachment" {
   count      = var.cfgs.utility_account_id == var.aws_account_id ? 1 : 0
-  role       = aws_iam_role.ots_role[count.index].name
+  role       = aws_iam_role.default_role[count.index].name
   policy_arn = aws_iam_policy.figgy_ots_policy[count.index].arn
 }
