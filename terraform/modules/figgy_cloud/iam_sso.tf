ee8bb0a1532da426164ae0bd19fa99d9c94866a1
Jordan Mance <mancej@snagajob.com>
2021-09-10 10:27:07 -0400

    Adding ots role

--- terraform/modules/figgy_cloud/iam_sso.tf
+++ terraform/modules/figgy_cloud/iam_sso.tf
@@ -59,5 +59,5 @@ resource "aws_iam_role_policy_attachment" "figgy_access_policy_attachment" {
 resource "aws_iam_role_policy_attachment" "figgy_ots_policy_attachment" {
   count      = var.cfgs.utility_account_id == var.aws_account_id ? 1 : 0
   role       = "figgy-ots"
-  policy_arn = aws_iam_policy.figgy_ots_policy.arn
+  policy_arn = aws_iam_policy.figgy_ots_policy[count.index].arn
 }
