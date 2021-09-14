ecc52b24226dea6019d91d46c1ab204f82a9de83
Jordan Mance <mancej@snagajob.com>
2021-09-14 12:46:32 -0400

    Adding ots role

--- terraform/modules/figgy_cloud/iam_generic_roles.tf
+++ terraform/modules/figgy_cloud/iam_generic_roles.tf
@@ -6,9 +6,36 @@
 resource "aws_iam_role" "default_role" {
   count                = var.cfgs.utility_account_id == var.aws_account_id && var.primary_region ? 1 : 0
   name                 = "figgy-default"
-  assume_role_policy   = var.cfgs.enable_sso ? data.aws_iam_policy_document.sso_role_policy[count.index].json : data.aws_iam_policy_document.bastion_role_policy[count.index].json
+  assume_role_policy   = var.cfgs.enable_sso ? data.aws_iam_policy_document.sso_role_policy[count.index].json : data.aws_iam_policy_document.figgy_bastion_default_role_policy.json
   max_session_duration = var.max_session_duration
 }
+
+# Role policy to allow cross-account assumption from bastion account
+data "aws_iam_policy_document" "figgy_bastion_default_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        for role in var.cfgs.role_types:
+          "arn:aws:iam::${var.cfgs.bastion_account_number}:role/figgy-${role}"
+      ]
+    }
+
+    dynamic "condition" {
+      for_each = var.cfgs.mfa_enabled ? [true] : []
+      content {
+        test     = "Bool"
+        values   = [var.cfgs.mfa_enabled]
+        variable = "aws:MultiFactorAuthPresent"
+      }
+    }
+  }
+}
+
 
 
 resource "aws_iam_role_policy_attachment" "figgy_ots_policy_attachment" {
